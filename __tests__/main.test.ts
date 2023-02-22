32c0cac5baf45694c6821ecc81c8769267d0f6c6
cjl_macbook <cjl_spy@163.com>
2023-02-22 21:10:50 +0800

    Test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,9 +2,22 @@ import {expect, test} from '@jest/globals'
 import {checkPrContentIgnoreChangelog} from '../src/util'
 
 test('Test', () => {
+  expect(checkPrContentIgnoreChangelog('1')).toBe(false)
+})
+
+test('Test default change log regex', () => {
   expect(
     checkPrContentIgnoreChangelog('Exempt CHANGELOG changes: The pr is action')
+  ).toBe(true)
+})
+
+test('Test custom change log regex', () => {
+  const customRegex = /ignore:.+/
+  expect(checkPrContentIgnoreChangelog('ignore changelog', customRegex)).toBe(
+    false
   )
 
-  expect(!checkPrContentIgnoreChangelog('1'))
+  expect(checkPrContentIgnoreChangelog('ignore: changelog', customRegex)).toBe(
+    true
+  )
 })
